912421a3463f4f9217069b6ecd8b3fd16a95a495
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-05-13 22:38:43 +0700

    Update_bottom_navbar_13_5

--- ExpenseManagement/app/src/main/java/com/example/expensemanagement/HomeActivity.java
+++ ExpenseManagement/app/src/main/java/com/example/expensemanagement/HomeActivity.java
@@ -1,19 +1,27 @@
 package com.example.expensemanagement;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private RadioGroup mRadioGroup;
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frame_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,33 +34,65 @@ public class HomeActivity extends AppCompatActivity {
             return insets;
         });
 
-        mRadioGroup = findViewById(R.id.lnbottom);
-        // Măc định cho ban đầu là home
-        switchFragment(new HomeFragment());
-        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frame_layout = findViewById(R.id.frameLayout);
+
+//        // Măc định cho ban đầu là home
+//        switchFragment(new HomeFragment());
+//        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                switch (checkedId) {
+//                    case R.id.rbHome:
+//                        switchFragment(new HomeFragment());
+//                        break;
+//                    case R.id.rbBill:
+//                        switchFragment(new BillFragment());
+//                        break;
+//                    case R.id.rbStore:
+//                        switchFragment(new StoreFragment());
+//                        break;
+//                    case R.id.rbAdmin:
+//                        switchFragment(new AdminFragment());
+//                        break;
+//                }
+//            }
+//        });
+
+        loadFragment(new HomeFragment(),true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.rbHome:
-                        switchFragment(new HomeFragment());
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemID = menuItem.getItemId();
+
+                switch (itemID) {
+                    case R.id.navHome:
+                        loadFragment(new HomeFragment(),false);
                         break;
-                    case R.id.rbBill:
-                        switchFragment(new BillFragment());
+                    case R.id.navBill:
+                        loadFragment(new BillFragment(),false);
                         break;
-                    case R.id.rbStore:
-                        switchFragment(new StoreFragment());
+                    case R.id.navStore:
+                        loadFragment(new StoreFragment(),false);
                         break;
-                    case R.id.rbAdmin:
-                        switchFragment(new AdminFragment());
+                    case R.id.navAdmin:
+                        loadFragment(new AdminFragment(),false);
                         break;
                 }
+
+                return true;
             }
         });
     }
 
-    private void switchFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.rootLayout, fragment);
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        }else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
         fragmentTransaction.commit();
     }
 }
